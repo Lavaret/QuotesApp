900ca9bf28f19d51a900afe67e7aed0275b516aa
Lavaret <czarnynozyk@gmail.com>
2025-03-26 21:30:21 +0100

    quote design

--- components/Quote.vue
+++ components/Quote.vue
@@ -1,5 +1,21 @@
 <template>
-  <span class="border border-stone-800 m-auto rounded p-3">
-    <slot />
-  </span>
+  <div class="border border-stone-400 m-auto rounded p-3 mx-3">
+    <div class="flex justify-end gap-2 text-xl">
+      <Icon name="solar:share-bold" class="cursor-pointer" style="color: black" />
+      <Icon name="solar:heart-bold" class="cursor-pointer" style="color: hotpink" />
+    </div>
+    <div class="text-left text-xl m-5">
+      <Icon name="ri:double-quotes-l" class="text-stone-500"/>
+      <slot name="content"/>
+    </div>
+    <div class="flex justify-end gap-2 items-center">
+      <Icon name="teenyicons:minus-solid" class="text-stone-500 text-lg"/>
+      <div>
+        <slot name="author"/>
+      </div>
+    </div>
+    <div class="text-right">
+      <slot name="source"/>
+    </div>
+  </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,56 @@
 <template>
-  <div class="bg-stone-200 min-h-32 w-100 flex flex-col gap-12">
-    <div class="font-bold p-3">
-      <h1 class="header-1 ">Welcome to the homepage</h1>
+  <div class="flex m-2">
+    <div class="bg-stone-300 w-1/4"></div>
+    <div class="bg-stone-200 min-h-32 w-1/2 flex flex-col gap-12">
+      <div class="font-bold p-3">
+        <h1 class="header-1 ">Welcome to the homepage</h1>
+      </div>
+      <div class="space-y-4">
+        <Quote>
+          <template #author>
+            Albert Einstein
+          </template>
+          <template #content>
+            If you can't explain it simply, you don't understand it well enough.
+          </template>
+        </Quote>
+
+        <Quote>
+          <template #author>
+            Lara Croft
+          </template>
+          <template #content>
+            Czasami odwaga jednego może przeważyć nad tchórzostwem wielu
+          </template>
+        </Quote>
+
+        <Quote>
+          <template #author>
+            dr sayer
+          </template>
+          <template #content>
+            Przepraszam, gdybyś miał racje, tobym się z Tobą zgodził
+          </template>
+          <template #source>
+            przebudzenia (film)
+          </template>
+        </Quote>
+
+
+        <Quote>
+          <template #content>
+            Jeszcze jedną niezwykłą cechą jego wykładów było to, że nie unikał słów "nie wiemy". "Nie wiemy" było kierunkowskazem
+            do odkrycia nowej prawdy, a nie wstydliwym przyznaniem się do niewiedzy. Wskazywanie terenów jeszcze nieodkrytych
+            miało dla niego taką samą wartość jak nauczanie o tym, co już zostało udowodnione.
+          </template>
+          <template #source>
+            Ukochane równanie profesora
+          </template>
+        </Quote>
+
+      </div>
     </div>
-    <div class="m-auto text-center">
-      <Quote>
-        “If you can't explain it simply, you don't understand it well enough.”
-        — Albert Einstein.
-      </Quote>
-    </div>
+    <div class="bg-stone-300 w-1/4"></div>
 
   </div>
 </template>
